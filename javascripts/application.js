17bfa7f19844f12d721f20d5dca5f42c4918ec37
Muhammad-Ismaeel <s10219144@connect.np.edu.sg>
2022-02-18 17:12:03 +0800

    started on javascript, created all files necessary

---
+++ javascripts/application.js
@@ -0,0 +1,95 @@
+const country_name_element = document.querySelector(".country .name");
+const total_cases_element = document.querySelector(".total-cases .value");
+const new_cases_element = document.querySelector(".total-cases .new-value");
+const recovered_element = document.querySelector(".recovered .value");
+const new_recovered_element = document.querySelector(".recovered .new-value");
+const deaths_element = document.querySelector(".deaths .value");
+const new_deaths_element = document.querySelector(".deaths .new-value");
+
+const ctx = document.getElementById("axes_line_chart").getContext("2d");
+
+
+let app_data = [],
+  cases_list = [],
+  recovered_list = [],
+  deaths_list = [],
+  deaths = [],
+  formatedDates = [];
+
+
+fetch("https://api.ipgeolocation.io/ipgeo?apiKey=14c7928d2aef416287e034ee91cd360d")
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    let country_code = data.country_code2;
+    let user_country;
+    country_list.forEach((country) => {
+      if (country.code == country_code) {
+        user_country = country.name;
+      }
+    });
+    fetchData(user_country);
+  });
+
+function fetchData(country) {
+  user_country = country;
+  country_name_element.innerHTML = "Loading...";
+
+  (cases_list = []),
+    (recovered_list = []),
+    (deaths_list = []),
+    (dates = []),
+    (formatedDates = []);
+
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  const api_fetch = async (country) => {
+    await fetch(
+      "https://api.covid19api.com/total/country/" + country + "/status/confirmed",
+      requestOptions
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        data.forEach((entry) => {
+          dates.push(entry.Date);
+          cases_list.push(entry.Cases);
+        });
+      });
+
+    await fetch(
+      "https://api.covid19api.com/total/country/" + country + "/status/recovered",
+      requestOptions
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        data.forEach((entry) => {
+          recovered_list.push(entry.Cases);
+        });
+      });
+
+    await fetch(
+      "https://api.covid19api.com/total/country/" + country + "/status/deaths",
+      requestOptions
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        data.forEach((entry) => {
+          deaths_list.push(entry.Cases);
+        });
+      });
+
+    updateUI();
+  };
+
+  api_fetch(country);
+}
